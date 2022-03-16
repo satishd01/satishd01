307b91635184311a2a381f58a8f0bf2a902978b1
satishd01 <85147362+satishd01@users.noreply.github.com>
2022-03-16 13:43:15 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+Int main(){
+Printf("hello);
+   }
+<!---
+satishd01/satishd01 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
